4b22c0dc52822eba47f942c76237802ed4e3e1b2
kengoyoshida <hukugonke@gmail.com>
2022-12-22 15:18:41 +0900

    created_others_profile_page_and_link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'others_profiles/show'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get 'events/index'
   root 'events#index'
@@ -41,4 +42,6 @@ Rails.application.routes.draw do
       resources :events, only: %i[index]
     end
   end
+
+  resources :others_profiles, only: %i[show]
 end
---
+++ spec/decorators/others_profile_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe OthersProfileDecorator do
+end
